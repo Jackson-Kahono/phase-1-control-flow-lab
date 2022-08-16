7dbc29765b95ab63938acaae8fb2877f31608269
Jackson-Kahono <jackson.kahono@student.moringaschool.com>
2022-08-16 20:20:13 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,11 +1,39 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(distance){
   // Write your code here!
+  if (distance <= 400) {
+    return "This one is on me!";
+  }
+  if (distance>2000 && distance<=2500) {
+    return "I will gladly take your thirty bucks."
+  }
+  if (distance>2500){
+    return "No can do."
+  }
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(x){
   // Write your code here!
+  switch (x) {
+    case 'NYC':
+      return 'Ok, sounds good.';
+      break;
+    case 'Pittsburgh':
+      return 'No go.';
+      break;
+  }
+
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(x){
   // Write your code here!
+  switch(x){
+    case 'generous':
+      return 'Thank you so much.';
+      break;
+    case 'not as generous':
+      return 'Thank you.';
+      break;
+    default:
+      return 'Bye.';
+  }
 }
